a24348e7ae77442cdeadebfddeac66901fa2a1b1
Muthukuda-Arachchige-ND <130959850+Muthukuda-Arachchige-ND@users.noreply.github.com>
2024-05-12 22:55:56 +0530

    Finished task management

---
+++ app/src/main/java/com/example/taskmanagement/Note.kt
@@ -0,0 +1,6 @@
+package com.example.taskmanagement
+
+import android.app.assist.AssistContent
+import android.icu.text.CaseMap.Title
+
+data class Note(val id:Int,val title: String,val content: String)
